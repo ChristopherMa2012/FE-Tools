8018b2c404e8f17fc7967e9819773214cfe3f5b8
christopherma2012 <christopherma2012@hotmail.com>
2018-06-01 08:45:00 +0800

    新增css记录

---
+++ CSS/css效果实现代码集合.md
@@ -0,0 +1,16 @@
+/*
+ * @Author: chris 
+ * @Date: 2018-06-01 08:33:58 
+ * @Last Modified by: chris
+ * @Last Modified time: 2018-06-01 08:43:26
+ */
+### 1、多行后添加省略号   
+```css  
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    -webkit-line-clamp: 5;   
+
+```
+> 实际效果  
+![实际效果](./referencePicture/ellipsis.PNG)
--- JS/数据请求工具/fetch.js
+++ JS/数据请求工具/fetch.js
@@ -1,8 +1,8 @@
 /*
  * @Author: chris 
  * @Date: 2018-05-31 19:58:30 
- * @Last Modified by:   chris 
- * @Last Modified time: 2018-05-31 19:58:30 
+ * @Last Modified by: chris
+ * @Last Modified time: 2018-05-31 20:38:45
  */
 (function(window){
   // opts = {
@@ -12,8 +12,15 @@
   //   withCredentials:''
   // }
     window.fetch = function(opts){
+      
        if( !!window.XMLHttpRequest){
          var xhr = new XMLHttpRequest();
+         xhr.withCredentials = opts.withCredentials;
+         xhr.open(opts.method,opts.url,opts.type);
+         xhr.onreadystatechange = function(){
+
+         }
+         xhr.send();
        }
     }
 })(window)
