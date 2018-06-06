73eab74cc65c8bf3a9cc45e2f0bf87d59b6c0b04
christopherma2012 <christopherma2012@hotmail.com>
2018-06-06 23:50:37 +0800

    js兼容及请求工具优化

--- JS/compatible/js-compatible.js
+++ JS/compatible/js-compatible.js
@@ -1,8 +1,8 @@
 /*
  * @Author: chris 
  * @Date: 2018-05-31 19:59:15 
- * @Last Modified by:   chris 
- * @Last Modified time: 2018-05-31 19:59:15 
+ * @Last Modified by: chris
+ * @Last Modified time: 2018-06-06 23:48:43
  */
 (function (window, document) {
   window.tagsStr = 'html,head,body,title,meta,style,script,link,div,span,img,a,abbr,acronym,'
@@ -14,46 +14,173 @@
     + 'meter,nav,noframes,noscript,object,ol,optgroup,option,output,p,param,pre,progress,'
     + 'q,rp,rt,ruby,s,samp,section,select,small,source,strike,strong,sub,table,tbody,td,'
     + 'textarea,tfoot,th,thead,time,tr,track,tt,u,ul,var,video,wbr';
-    
+
   // 数组indexOf方法兼容
   !Array.prototype.indexOf && (Array.prototype.indexOf = function (targetEle, fromIndex) {
-    var k
+    var k;
     if (this == null) {
-      throw new TypeError('"this" is null or not defined')
+      throw new TypeError('"this" is null or not defined');
     }
-    var O = Object(this)
-    var len = O.length >>> 0
+    var O = Object(this);
+    var len = O.length >>> 0;
     if (len === 0) {
-      return -1
+      return -1;
     }
-    var n = +fromIndex || 0
+    var n = +fromIndex || 0;
     if (Math.abs(n) === Infinity) {
-      n = 0
+      n = 0;
     }
     if (n >= len) {
-      return -1
+      return -1;
     }
-    k = Math.max(n >= 0 ? n : len - Math.abs(n), 0)
+    k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
     while (k < len) {
       if (k in O && O[k] === targetEle) {
-        return k
+        return k;
       }
-      k++
+      k++;
     }
-    return -1
-  })
+    return -1;
+  });
 
   //  数组forEach方法兼容
   !Array.prototype.forEach && (Array.prototype.forEach = function (callback) {
     for (var i = 0; i < this.length; i++) {
-      callback(this[i], i, this)
+      callback(this[i], i, this);
     }
-  })
+  });
 
   // 数组includes方法兼容
   !Array.prototype.includes && (Array.prototype.includes = function (targetEle) {
-    return Array.prototype.indexOf.call(this,targetEle) > 0 ? true : false;
-  })
+    return Array.prototype.indexOf.call(this, targetEle) > 0 ? true : false;
+  });
+
+  //Object.keys兼容
+  if (typeof Object.keys !== 'function') {
+    Object.keys = function (targetObj) {
+      var temArr = [];
+      for (var key in targetObj) {
+        if (targetObj.hasOwnProperty(key)) {
+          temArr.push(key);
+        }
+      }
+      return temArr;
+    };
+  }
+
+  //Object.defineProerties兼容
+  if (typeof Object.defineProerties !== 'function') {
+    Object.defineProperties = function (obj, properties) {
+      function convertToDescriptor (desc) {
+        function hasProperty (obj, prop) {
+          return Object.prototype.hasOwnProperty.call(obj, prop);
+        }
+
+        function isCallable (v) {
+          // NB: modify as necessary if other values than functions are callable.
+          return typeof v === 'function';
+        }
+
+        if (typeof desc !== 'object' || desc === null)
+          throw new TypeError('bad desc');
+
+        var d = {};
+
+        if (hasProperty(desc, 'enumerable'))
+          d.enumerable = !!desc.enumerable;
+        if (hasProperty(desc, 'configurable'))
+          d.configurable = !!desc.configurable;
+        if (hasProperty(desc, 'value'))
+          d.value = desc.value;
+        if (hasProperty(desc, 'writable'))
+          d.writable = !!desc.writable;
+        if (hasProperty(desc, 'get')) {
+          var g = desc.get;
+
+          if (!isCallable(g) && typeof g !== 'undefined')
+            throw new TypeError('bad get');
+          d.get = g;
+        }
+        if (hasProperty(desc, 'set')) {
+          var s = desc.set;
+          if (!isCallable(s) && typeof s !== 'undefined')
+            throw new TypeError('bad set');
+          d.set = s;
+        }
+
+        if (('get' in d || 'set' in d) && ('value' in d || 'writable' in d))
+          throw new TypeError('identity-confused descriptor');
+
+        return d;
+      }
+
+      if (typeof obj !== 'object' || obj === null)
+        throw new TypeError('bad obj');
+
+      properties = Object(properties);
+
+      var keys = Object.keys(properties);
+      var descs = [];
+
+      for (var i = 0; i < keys.length; i++)
+        descs.push([keys[i], convertToDescriptor(properties[keys[i]])]);
+
+      for (var i = 0; i < descs.length; i++)
+        Object.defineProperty(obj, descs[i][0], descs[i][1]);
+
+      return obj;
+    };
+  }
+
+  //Object.create兼容
+  if (typeof Object.create !== 'function') {
+    Object.create = function (proto, propertiesObject) {
+      if (typeof proto !== 'object' && typeof proto !== 'function') {
+        throw new TypeError('Object prototype may only be an Object: ' + proto);
+      }
+      else if (proto === null) {
+        throw new Error('This browser\'s implementation of Object.create is a shim and doesn\'t support \'null\' as the first argument.');
+      }
+      if (typeof propertiesObject != 'undefined') {
+        throw new Error('This browser\'s implementation of Object.create is a shim and doesn\'t support a second argument.');
+      }
+
+      function F () { }
+      F.prototype = proto;
+
+      return new F();
+    };
+  }
 
 
-})(window, document)
+  //bind兼容
+  if (!Function.prototype.bind) {
+    Function.prototype.bind = function (oThis) {
+      if (typeof this !== 'function') {
+        // closest thing possible to the ECMAScript 5
+        // internal IsCallable function
+        throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
+      }
+
+      var aArgs = Array.prototype.slice.call(arguments, 1),
+        fToBind = this,
+        fNOP = function () { },
+        fBound = function () {
+          return fToBind.apply(this instanceof fNOP
+            ? this
+            : oThis,
+            // 获取调用时(fBound)的传参.bind 返回的函数入参往往是这么传递的
+            aArgs.concat(Array.prototype.slice.call(arguments)));
+        };
+
+      // 维护原型关系
+      if (this.prototype) {
+        // Function.prototype doesn't have a prototype property
+        fNOP.prototype = this.prototype;
+      }
+      fBound.prototype = new fNOP();
+
+      return fBound;
+    };
+  }
+})(window, document);
--- JS/requestTool/fetch.js
+++ JS/requestTool/fetch.js
@@ -2,15 +2,29 @@
  * @Author: chris 
  * @Date: 2018-05-31 19:58:30 
  * @Last Modified by: chris
- * @Last Modified time: 2018-06-05 01:28:48
+ * @Last Modified time: 2018-06-06 23:49:27
  */
 (function (window) {
   var obtain = {};
-  //请求初始设置
-  obtain.setting = {
-    xhrDefaultInitCount: 2,
-    timeout: 8000
-  };
+  obtain.setting = {};
+
+  function defineProSetting (callback) {
+    return {
+      enumerable: true,
+      set: function (newVal) {
+        this.value = newVal;
+        callback && callback();
+      },
+      get: function () {
+        return this.value;
+      }
+    };
+  }
+  Object.defineProperties(obtain.setting, {
+    xhrDefaultInitCount: defineProSetting(xhrPoolInit),
+    timeout: defineProSetting()
+  });
+
   //执行环境
   obtain.isXdr = window.navigator.userAgent.indexOf('MSIE 9.0') > 0 || window.navigator.userAgent.indexOf('MSIE 8.0') > 0;
   //xhr对象池
@@ -18,12 +32,17 @@
   //空闲xhr对象索引
   obtain.freeXhrIndexArr = [];
 
-  //xhr对象池初始化 
-  xhrPoolInit();
+  //请求初始设置 - 初始化xhr池
+  obtain.setting.xhrDefaultInitCount = 2;
+  obtain.setting.timeout = 8000;
+  window.navigator.userAgent.indexOf('MSIE 8.0') > 0 && xhrPoolInit();
+
 
   //xhr对象池初始化
   function xhrPoolInit () {
     var len = obtain.setting.xhrDefaultInitCount;
+    obtain.xhrPool = [];
+    obtain.freeXhrIndexArr = [];
     for (var i = 0; i < len; i++) {
       obtain.xhrPool.push(getXhr());
       obtain.freeXhrIndexArr.push(i);
@@ -50,6 +69,7 @@
       xdr.timeout = obtain.setting.timeout;
       xdr.ontimeout = function () {
         console.error('请求超时');
+        xdr.abort();
       };
       xdr.onprogress = function (res) {
         console.log(res);
@@ -66,17 +86,33 @@
     }
   }
 
+  function objToStr (data) {
+    if (data == null || data == undefined) return;
+    var str = '';
+    for (var key in data) {
+      if (data.hasOwnProperty(key)) {
+        str += key + '=' + data[key] + '&';
+      }
+    }
+    return str.slice(0, -1);
+  }
+
   //使用xdr发起请求
   function xdrOpen (opts) {
+    if (opts.method === 'post') {
+      console.error('XDomainRequest 不支持post请求方式传递参数！');
+      return;
+    }
     var xdr = getFreeXhrFromXhrPool();
     xdr.onload = function () {
-      opts.callback(JSON.parse(xdr.responseText));
+      opts.success(JSON.parse(xdr.responseText));
       obtain.freeXhrIndexArr.push(obtain.xhrPool.indexOf(xdr));
-      console.log(obtain.xhrPool.indexOf(xdr));
     };
-    xdr.open(opts.method, opts.url + '?random=' + Math.random());
+    opts.url += opts.data == undefined ? '' : '?' + encodeURIComponent(objToStr(opts.data));
+    console.log(opts.url);
+    xdr.open(opts.method, opts.url);
     setTimeout(function () {
-      xdr.send(opts.data);
+      xdr.send();
     }, 0);
   }
 
@@ -85,14 +121,14 @@
     var xhr = getFreeXhrFromXhrPool();
     xhr.onreadystatechange = function () {
       if (xhr.readyState == 4 && xhr.status == 200) {
-        opts.callback(JSON.parse(xhr.responseText));
+        opts.success(JSON.parse(xhr.responseText));
         obtain.freeXhrIndexArr.push(obtain.xhrPool.indexOf(xhr));
         console.log(obtain.xhrPool.indexOf(xhr));
       }
     };
-    xhr.open(opts.method, opts.url + '?random=' + Math.random());
+    xhr.open(opts.method, opts.url);
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.send(opts.data);
+    xhr.send(objToStr(opts.data));
   }
 
   //请求方法request
@@ -101,13 +137,26 @@
   };
 
   //请求方法request
-  obtain.get = function (url) {
+  obtain.get = function (url, data, successCB, failCB) {
     var opts = {
       method: 'get',
-      url: url
+      url: url,
+      data: data,
+      success: successCB,
+      fail: failCB
     };
     this.isXdr ? xdrOpen(opts) : xhrOpen(opts);
   };
 
+  obtain.post = function (url, data, successCB, failCB) {
+    var opts = {
+      method: 'post',
+      url: url,
+      data: data,
+      success: successCB,
+      fail: failCB
+    };
+    this.isXdr ? xdrOpen(opts) : xhrOpen(opts);
+  };
   window.obtain = obtain;
 })(window);
