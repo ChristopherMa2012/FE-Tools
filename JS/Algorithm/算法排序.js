3d15b7e65cde461ad36ff30cedd377eb2703bdd3
christopherma2012 <christopherma2012@hotmail.com>
2018-07-04 10:20:47 +0800

    新增快速排序、选择排序、冒泡排序

--- JS/Algorithm/算法排序.js
+++ JS/Algorithm/算法排序.js
@@ -1,3 +1,4 @@
+//快速排序
 function quickSort(arr, startIndex, endIndex) {
 	if (arr.length == 0 || endIndex - startIndex < 1) return;
 	var i = startIndex, j = endIndex, base = arr[startIndex], tem;
@@ -13,8 +14,36 @@ function quickSort(arr, startIndex, endIndex) {
 		}
 	}
 	tem = arr[j]; arr[j] = arr[startIndex]; arr[startIndex] = tem;
-	console.log(arr)
 	quickSort(arr, startIndex, i - 1);
 	quickSort(arr, i + 1, endIndex);
+	return arr;
+}
+
+//选择排序
+function selectSort(arr){
+	var tem;
+	for(var i = 0; i < arr.length - 1; i++){
+		for(var j = i + 1; j < arr.length;j ++){
+			if(arr[i] > arr[j]){
+				tem = arr[i]; arr[i] = arr[j]; arr[j] = tem;
+			}
+		}
+	}
+	return arr;
+}
+
+//冒泡排序
+function bubbleSort(arr){
+	var tem,count = 0;
+  for(var i = 0; i < arr.length; i ++){
+		count = 0;
+		for(var j = 0; j < arr.length - 1; j++){
+       if(arr[j] > arr[j+1]){
+				 tem = arr[j];arr[j] = arr[j+1]; arr[j+1] = tem;
+				 count++;
+			 }
+		}
+		if(count <=1) break;
+	}
 	return arr;
 }
