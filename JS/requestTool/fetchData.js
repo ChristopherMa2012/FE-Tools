420011a05a507e6e1b21a7c6ccca076611437e4f
ma <1367139690@qq.com>
2018-08-14 15:35:18 +0800

    fetch请求工具完善

--- JS/requestTool/fetchData.js
+++ JS/requestTool/fetchData.js
@@ -1,6 +1,5 @@
+
 //异步请求工具函数
-
-
 /*opts = {
   method: '',  // get/post
   mode: '',
@@ -12,55 +11,63 @@
   或者headers: new Headers()
   body: FormData 或者json对象
 }*/
+// let data = null, error = null;
 export default async opts => {
   let optsData = Object.create(null);
   // optsData.mode = 'cors';
   // optsData.credentials = 'inlucde';
   // optsData.cache = 'no-cache';
 
-//get请求方式
-if (opts.method.toUpperCase() === 'GET') {
-  optsData.method = 'GET';
-  if (opts.body != undefined) {
-    opts.url += '?';
-    opts.body && Object.keys(opts.body).forEach(item => {
-      opts.url += `${item}=${opts.body[item]}&`
-    })
-    opts.url = opts.url.slice(0, -1)
+  //get请求方式
+  if (opts.method && opts.method.toUpperCase() === 'GET') {
+    optsData.method = 'GET';
+    if (opts.body !== undefined) {
+      opts.url += '?';
+      opts.body && Object.keys(opts.body).forEach(item => {
+        opts.url += `${item}=${opts.body[item]}&`
+      })
+      opts.url = opts.url.slice(0, -1)
+    }
   }
-}
-//post请求方式
-if (opts.method.toUpperCase() === 'POST') {
-  optsData.method = 'POST';
-  //json数据
-  if (Object.prototype.toString.call(opts.body) !== "[object FormData]") {
-    optsData.body = JSON.stringify(opts.body);
-    optsData.headers = {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
+  //post请求方式
+  if (opts.method && opts.method.toUpperCase() === 'POST') {
+    optsData.method = 'POST';
+    //json数据
+    if (Object.prototype.toString.call(opts.body) !== "[object FormData]") {
+      optsData.body = JSON.stringify(opts.body);
+      optsData.headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+      //form表单
+    } else {
+      optsData.body = opts.body;
+      optsData.headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
     }
-    //form表单
-  } else {
-    optsData.body = opts.body;
-    optsData.headers = {
-      'Accept': 'application/json',
-      'Content-Type': 'application/x-www-form-urlencoded'
-    }
+  }
 
-  }
-}
-// Object.keys(optsData).forEach(item => {
-//   if (opts.optsData.hasOwnProperty(item)) return;
-//   opts.optsData[item] = optsData[item]
-// });
-console.log(optsData)
-const res = await fetch(opts.url, {
+  // try {
+  const res = await fetch(opts.url, {
     mode: 'cors',
     credentials: 'include',
-    method: optsData.method,
+    method: optsData.method || 'GET',
     headers: optsData.headers,
-    body: optsData.body || {}
+    body: optsData.body || undefined
   });
-const resJson = await res.json();
-return resJson;
+  let errLabel = true;
+  let data = null;
+  try {
+    data = await res.json()
+  } catch (err) {
+    errLabel = false;
+    /**
+     * 请求失败处理操作
+     */
+  } finally {
+    if (errLabel) return data;
+  }
 }
+
